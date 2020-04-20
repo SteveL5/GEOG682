88af619be9bd650e09ddce18ec5fa3af8da29e4f
SteveL5 <61533523+SteveL5@users.noreply.github.com>
2020-04-21 03:32:44 +0900

    Update Littel_Lab 4.py

--- Littel_Lab 4.py
+++ Littel_Lab 4.py
@@ -3,7 +3,8 @@ crime = "S:/682/Spring20/slittel/Lab 4/Crime/Crime_Incidents_in_2017/Crime_Incid
 districts = "S:/682/Spring20/slittel/Lab 4/Districts/Police_Districts/Police_Districts.shp"
 iface.addVectorLayer(crime,"crime","ogr")
 iface.addVectorLayer(districts,"districts","ogr")
-processing.run("qgis:joinbylocationsummary", {'INPUT':districts,'JOIN':crime,'PREDICATE':1,'SUMMARIES':0,'OUTPUT':"S:/682/Spring20/slittel/Lab 4/Lab_4_join.shp"})
+processing.run("qgis:joinbylocationsummary", 
+               {'INPUT':districts,'JOIN':crime,'PREDICATE':1,'SUMMARIES':0,'OUTPUT':"S:/682/Spring20/slittel/Lab 4/Lab_4_join.shp"})
 joindata = "S:/682/Spring20/slittel/Lab 4/Lab_4_join.shp"
 iface.addVectorLayer(joindata,"joindata","ogr")
 #The 3rd Police District had the most crime with a count if 5,970
